400f47580b3985388257413660cc3508fbea2133
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-10-09 09:07:20 -0300

    Add files via upload

---
+++ CapsManFy/Login/script.js
@@ -0,0 +1,85 @@
+let conta = null;
+let submitCadastro = document.querySelector('#register-form .button');
+let submitLogin = document.querySelector('#login-form .button');
+
+let users = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+function addUser(event) {
+    event.preventDefault();
+    let name = document.querySelector('#nameInput2').value;
+    let email = document.querySelector('#emailInput2').value;
+    let password = document.querySelector('#passwordInput2').value;
+    let confirmPassword = document.querySelector('#confirmarSenha').value;
+    let errorMessage = document.querySelector("#erroMensagem");
+    errorMessage.style.display = 'none';
+    errorMessage.innerHTML = '';
+
+    if (!email || !password) {
+        errorMessage.innerHTML = "Email e senha são obrigatórios.";
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        errorMessage.innerHTML = "As senhas não são iguais.";
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    let usuarioExistente = users.find(user => user.email === email);
+    if (usuarioExistente) {
+        errorMessage.innerHTML = "Já existe um usuário com esse email.";
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    conta = { name, email, senha: btoa(password), playlist: [] };
+    users.push(conta);
+    localStorage.setItem('usuarios', JSON.stringify(users));
+    console.log("Cadastro realizado com sucesso!");
+
+    document.querySelector('#nameInput2').value = '';
+    document.querySelector('#emailInput2').value = '';
+    document.querySelector('#passwordInput2').value = '';
+    document.querySelector('#confirmarSenha').value = '';
+}
+
+function loginUser(event) {
+    event.preventDefault();
+    let email = document.getElementById('emailInput1').value;
+    let password = document.getElementById('passwordInput1').value;
+    let errorMessage = document.querySelector("#erroMensagem");
+    errorMessage.style.display = 'none';
+    errorMessage.innerHTML = '';
+
+    let usuario = users.find(user => user.email === email);
+    if (!usuario) {
+        errorMessage.innerHTML = "Você precisa cadastrar primeiro.";
+        errorMessage.style.display = 'block';
+        return;
+    }
+
+    if (usuario.senha === password) {
+        window.location.href = "../home/index.html";
+    } else {
+        errorMessage.innerHTML = "Email ou senha incorretos.";
+        errorMessage.style.display = 'block';
+    }
+}
+
+
+
+document.getElementById('show-register').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('login-form').classList.add('hidden');
+    document.getElementById('register-form').classList.remove('hidden');
+});
+
+document.getElementById('show-login').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('register-form').classList.add('hidden');
+    document.getElementById('login-form').classList.remove('hidden');
+});
+
+document.querySelector('#buttonSubmit2').addEventListener('click', addUser);
+document.querySelector('#buttonSubmit1').addEventListener('click', loginUser);
